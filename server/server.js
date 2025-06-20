9c9b47a15957e237b269389c30d1ce755fc26187
Bhumit267 <bhumitprs15101975@gmail.com>
2025-06-21 00:05:23 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -2,18 +2,27 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/dbconfig');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
+// Load environment variables
 dotenv.config();
+
+// Initialize app
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors()); // Allow cross-origin requests (important for frontend)
 
-// Connect DB
+// Connect to DB
 connectDB();
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/users', userRoutes);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
